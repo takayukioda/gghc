9d9d03940f8b87af785bacfec6f0004ee6603473
takayukioda <takayuki@odashi.me>
2017-07-23 13:39:52 +0900

    Implement label listing command

--- main.go
+++ main.go
@@ -1,34 +1,59 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"os"
-	// "github.com/golang/x/oauth2"
-	// "github.com/google/go-github/github"
+
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Fprintln(os.Stderr, "this command requires at least one argument")
-		os.Exit(1)
-	}
-
 	user := flag.String("user", "", "GitHub username")
 	repo := flag.String("repo", "", "Repository name")
 	token := flag.String("token", "", "Access token")
 
 	flag.Parse()
 
+	// configuring github token
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: *token},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+
 	args := flag.Args()
 
-	target := args[0]
+	if len(args) < 2 {
+		fmt.Fprintf(os.Stderr, "usage: %s <resource> <action>\n", os.Args[0])
+		os.Exit(1)
+	}
 
+	target := args[0]
+	action := args[1]
 	switch target {
+	case "labels":
+		if action != "list" {
+			fmt.Fprintln(os.Stderr, "Undefined action:", action)
+			os.Exit(1)
+		}
+		labels, _, err := client.Issues.ListLabels(ctx, *user, *repo, nil)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		for _, l := range labels {
+			fmt.Println("Label:", l.String())
+		}
 	default:
 		fmt.Println("Option[user]:", *user)
 		fmt.Println("Option[repo]:", *repo)
 		fmt.Println("Option[token]:", *token)
 		fmt.Println("Target:", target)
 	}
+
+	os.Exit(0)
 }
